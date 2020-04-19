c8ad19610113aa50873292a8ebbdc0348f60154a
Dev-solutions100 <63174179+Dev-solutions100@users.noreply.github.com>
2020-04-18 22:16:52 -0700

    Update app.py

--- app.py
+++ app.py
@@ -435,12 +435,12 @@ def maps_search1(data):
             respo=respo.json()
             ##print("FREEEEEEEEEEEEEE")
             l=len(respo)
-            if(respo!=''):
+            try:
                 i1=respo[l-1].get("Confirmed")
                 i2=respo[l-1].get("Recovered")
                 i3=respo[l-1].get("Deaths")
                 r1=emoji.emojize(':globe_with_meridians:', use_aliases=True)+" *"+data1+" (Real Time)*\n\n"+emoji.emojize(':bar_chart:', use_aliases=True)+" Total cases: "+str(i1)+"\n"+emoji.emojize(':chart_with_upwards_trend:', use_aliases=True)+" Total recovery: "+str(i2)+"\n"+emoji.emojize(':chart_with_downwards_trend:', use_aliases=True)+" Total deaths: "+str(i3)+"\n\n"+emoji.emojize(':round_pushpin:', use_aliases=True)+"Reply with any country's name to see its cases (Example: *Spain*)\n"+emoji.emojize(':round_pushpin:', use_aliases=True)+"Reply with *0* for Main Menu"
-            else:
+            except:
                 r1="Please check the country's name (Ex: *United States of America*). Or try after sometime."
         else:
             r1="Please check the country's name (Ex: *United States of America*). Or try after sometime."
