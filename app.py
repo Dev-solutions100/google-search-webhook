219b8bd381cad960bebcd290614584f0e2d97697
Dev-solutions100 <63174179+Dev-solutions100@users.noreply.github.com>
2020-05-09 01:26:50 -0700

    Update app.py

--- app.py
+++ app.py
@@ -926,7 +926,7 @@ def maps_search1(data,num):
 #                 i4=respo[l-1].get("Country")
              
                 urlc="https://disease.sh/v2/countries"
-                headers={'yesterday':str(1)}
+                headers={'yesterday':str(true)}
                 respos = requests.request("GET",url,headers=headers)
                 print(respos)
                 respos=respos.json()
