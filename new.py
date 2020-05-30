43937bdbfb67903e7f24fc9554c373e7aeedeae4
Dev-solutions100 <63174179+Dev-solutions100@users.noreply.github.com>
2020-05-30 17:47:10 +0530

    Update new.py

--- new.py
+++ new.py
@@ -1,4 +1,5 @@
 import os
+from apscheduler.schedulers.background import BackgroundScheduler
 import requests
 from crontabs import Cron, Tab
 
@@ -9,11 +10,17 @@ def my_job():
 
 
 # Will run with a 10 second interval synced to the top of the minute
-Cron().schedule(Tab(name='run_my_job').every(seconds=10).run(my_job)).go()
+#Cron().schedule(Tab(name='run_my_job').every(seconds=10).run(my_job)).go()
 
 if __name__ == '__main__':
-    port = int(os.getenv('PORT', 5000))
+    #port = int(os.getenv('PORT', 5000))
 
     #print("Starting app on port %d" % port)
 
-    app.run(debug=False, port=port, host='0.0.0.0')
+    #app.run(debug=False, port=port, host='0.0.0.0')
+    
+    scheduler = BackgroundScheduler()
+    scheduler.configure(timezone=utc)
+    scheduler.add_job(my_job, 'interval', seconds=10)
+    scheduler.add
+    scheduler.start()
