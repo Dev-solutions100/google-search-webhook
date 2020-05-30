35b42f47dd625a019a8460f90bbe92c05bb4e7d9
Dev-solutions100 <63174179+Dev-solutions100@users.noreply.github.com>
2020-05-30 21:34:21 +0530

    Update new.py

--- new.py
+++ new.py
@@ -36,3 +36,9 @@ if __name__ == '__main__':
 
     while True:
         time.sleep(5)
+        
+    port = int(os.getenv('PORT', 5000))
+
+    print("Starting app on port %d" % port)
+
+    app.run(debug=False, port=port, host='0.0.0.0')
