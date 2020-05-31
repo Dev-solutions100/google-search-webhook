3c77e13a1e98db0f527f86925f3a6fabd53be0d1
Dev-solutions100 <63174179+Dev-solutions100@users.noreply.github.com>
2020-05-31 14:45:53 +0530

    Update new.py

--- new.py
+++ new.py
@@ -18,11 +18,21 @@ def my_job():
     g=github.Github("Dev-solutions100","kk202050")
     repo=g.get_user().get_repo("google-search-webhook")
     contents=repo.get_contents("test.txt")
+    contents1=repo.get_contents("zone.txt")
+    contents2=repo.get_contents("state.txt")
+    contents3=repo.get_contents("country.txt")
 #     str=str+" test"
     url = "https://api.covid19india.org/v2/state_district_wise.json"
+    url1= "https://api.covid19india.org/zones.json"
+    url2= "https://api.covid19india.org/data.json"
+    url3= "https://corona.lmao.ninja/v2/countries"
+    ck=0
     err=0
     try:
-        respo = requests.request("GET", url)
+        respo = requests.request("GET", url, timeout=5)
+        respo1 = requests.request("GET", url1, timeout=5)
+        respo2 = requests.request("GET", url2, timeout=5)
+        respo3 = requests.request("GET", url3, timeout=5)
 #         print("FREEEEEEEEEEEEEE")
 #         print(respo.status_code)
     except:
@@ -30,12 +40,21 @@ def my_job():
     finally:
         if(err==0):
             respo=respo.json()
-            repo.update_file(contents.path,"Updated",str(respo),contents.sha)
+            respo1=respo1.json()
+            respo2=respo2.json()
+            respo3=respo3.json()
+            try:
+                repo.update_file(contents.path,"Updated",str(respo),contents.sha)
+                repo.update_file(contents1.path,"Updated",str(respo1),contents1.sha)
+                repo.update_file(contents2.path,"Updated",str(respo2),contents2.sha)
+                repo.update_file(contents3.path,"Updated",str(respo3),contents3.sha)
+            except:
+                ck=1
 #     file1 = open("test.txt", "w")  # append mode 
 #     file1.write("Today") 
 #     file1.close()
-
-        print('abcd1234')
+            finally:
+                print('abcd1234')
 
 
 # Will run with a 10 second interval synced to the top of the minute
@@ -49,7 +68,7 @@ if __name__ == '__main__':
     #app.run(debug=False, port=port, host='0.0.0.0')
     
     scheduler = BackgroundScheduler()
-    scheduler.add_job(my_job, 'interval', seconds=1800)
+    scheduler.add_job(my_job, 'interval', seconds=3600)
     scheduler.start()
 
     while True:
