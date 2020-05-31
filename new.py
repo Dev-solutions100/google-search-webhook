8f119bb487dbc60018f48c902d4acac4fc5db9c3
Dev-solutions100 <63174179+Dev-solutions100@users.noreply.github.com>
2020-05-31 16:37:07 +0530

    Update new.py

--- new.py
+++ new.py
@@ -105,7 +105,7 @@ if __name__ == '__main__':
     #app.run(debug=False, port=port, host='0.0.0.0')
     
     scheduler = BackgroundScheduler()
-    scheduler.add_job(my_job, 'interval', seconds=60)
+    scheduler.add_job(my_job, 'interval', seconds=3600)
     scheduler.start()
 
     while True:
