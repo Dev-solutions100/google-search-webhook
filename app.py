9442550d7aa7f72e74b592f13ea3d723f1b67383
Dev-solutions100 <63174179+Dev-solutions100@users.noreply.github.com>
2020-05-31 16:22:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -428,7 +428,7 @@ def state(data,num):
     finally:
         if(err==0):
             ##print("FREEEEEEEEEEEEEE")
-            fl = open('state.txt')
+            fl = open('state.txt').read()
             
 #             fl=fl.replace('\"','\'')
 #             fl=fl.replace('{\'','{\"')
