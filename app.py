ddc3fc673421f67ad08418e1db573dc37291336b
Dev-solutions100 <63174179+Dev-solutions100@users.noreply.github.com>
2020-05-31 15:58:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -428,7 +428,15 @@ def state(data,num):
     finally:
         if(err==0):
             ##print("FREEEEEEEEEEEEEE")
-            fl = open('state.txt').read().replace("\'", "\"")
+            fl = open('state.txt').read()
+            
+            fl=fl.replace('{\'','{\"')
+            fl=fl.replace('\'}','\"}')
+            fl=fl.replace('\':','\":')
+            fl=fl.replace(': \'',': \"')
+            fl=fl.replace(', \'',', \"')
+            fl=fl.replace('\',','\",')
+            
             respo = json.loads(fl)
             #respo=respo.json()
             ##print("FREEEEEEEEEEEEEE")
